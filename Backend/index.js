202e2ac253ba344db9636208fe9bcbd3eb5da106
Piyush <piyushsainiyt868@gmail.com>
2025-03-18 23:17:37 +0530

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 
 app.use("/users", userRoutes);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
